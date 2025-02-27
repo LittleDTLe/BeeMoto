74767ca472acae99a7bb81551c97e227d16e5a6a
LittleDTLe <themiz39@gmail.com>
2025-02-27 20:08:53 +0200

    After Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Services from './Components/Services/Services'
@@ -7,8 +7,12 @@ import About from './Components/About/About'
 import Store from './Components/Store/Store'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import Video from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -16,13 +20,14 @@ const App = () => {
       <div className="container">
         <Title subTitle='our services' title='What We Offer'/>
         <Services/>
-        <About/>
+        <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Our Space'/>
         <Store/>
         <Title subTitle='Contact Us' title='Only a Message Away'/>
         <Contact/>
         <Footer/>
       </div>
+      <Video playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link, Element } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
 
@@ -13,16 +14,23 @@ const Navbar = () => {
     })
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky? 'dark_nav' : ''}`}>
         <img src={logo} alt="" className='logo' />
-        <ul>
+        <ul className={mobileMenu?'':'hide-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='services' smooth={true} offset={-300} duration={500}>Services</Link></li>
             <li><Link to='about' smooth={true} offset={-200} duration={500}>About</Link></li>
             <li><Link to='gallery' smooth={true} offset={-250} duration={500}>Gallery</Link></li>
             <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact Us</Link></li>
         </ul>
+        <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
