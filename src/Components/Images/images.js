9e9670471ab31f5b56fe4796ac821ea8634720f8
LittleDTLe <themiz39@gmail.com>
2025-03-10 22:56:50 +0200

    Services Page with Partial Scroll Functionality

--- src/Components/Images/images.js
+++ src/Components/Images/images.js
@@ -13,7 +13,12 @@ export const images = [
         url: "https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "Sport",
         description: "Sunset over the ocean"
-      },
+    },
+    {
+        url: "https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Sport",
+        description: "Sunset over the ocean"
+    },
     {
       url: "https://plus.unsplash.com/premium_photo-1664303751783-e18caf8f609d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Dirt",
